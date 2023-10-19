9ca36c713d9740b40de0f653ecdfc013de5b6278
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-10-19 13:16:11 +0300

    hioo

--- src/routes/todo/index.page.tsx
+++ src/routes/todo/index.page.tsx
@@ -10,7 +10,6 @@ import {
 import { createTodo, readAllTodos } from "src/crud";
 
 
-
 export default function TodoPage() {
   const { data, refetch } = useServerSideQuery(readAllTodos, {
     refetchOnWindowFocus: true,
